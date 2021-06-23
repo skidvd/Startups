84c9de0180e42ad2b89002b53f349c54b76b2dde
Todd Gould <todd@abilsoft.com>
2021-06-23 15:34:05 -0600

    Initial baseline

---
+++ calculator/src/footer/Footer.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import './Footer.css';
+
+import {ADDITION, SUBTRACTION, MULTIPLICATION, OPERATORS } from "../calculator/state/state";
+
+const Footer = (props) => {
+
+    const operatorClassName = (op) => {
+        switch (op) {
+            case ADDITION:
+                return 'add'
+            case SUBTRACTION:
+                return 'subtract'
+            case MULTIPLICATION:
+                return 'multiply';
+            default:
+                console.error(`Unsupported operator: ${op}`);
+                return '';
+        }
+    }
+    const operators = OPERATORS.map(op => (
+        <button className={operatorClassName(op)} onClick={e => {
+            e.preventDefault();
+            props.dispatch({type: 'operatorSelect', payload: op})
+        }} disabled={props.state.firstOperand === undefined || props.state.result}>{op}</button>
+    ));
+
+    return (
+        <div className="footer">
+            <div className="operators">
+                {operators}
+            </div>
+            <button className="calculate-button" onClick={e => {
+                e.preventDefault();
+                props.dispatch({type: 'calculate'})
+            }} disabled={props.state.secondOperand === undefined || props.state.result}>Calculate!</button>
+            <button className="clear-button" onClick={e => {
+                e.preventDefault();
+                props.dispatch({type: 'clear'})
+            }} disabled={props.state.firstOperand === undefined}>Clear</button>
+        </div>
+    );
+};
+
+export default Footer;
