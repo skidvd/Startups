f5b0f54841f15fff6ec9408e3723d1c8eb07a682
Todd Gould <todd@abilsoft.com>
2021-06-23 15:39:17 -0600

    Initial baseline

---
+++ calculator/src/header/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './Header.css';
+import ValueInput from "./valueInput/ValueInput";
+import ValueDisplay from "./valueDisplay/ValueDisplay";
+
+const Header = (props) => {
+    // We only display the elements of the calculation that have already been selected
+    let display = [<ValueInput value={props.state.firstOperand !== undefined ? props.state.firstOperand : ''} disabled={props.state.operator || props.state.result} key='firstOperand' dispatch={props.dispatch}></ValueInput>];
+    if (props.state.operator) {
+        display.push(<ValueDisplay value={props.state.operator} key='operator' className="operator"></ValueDisplay>)
+        display.push(<ValueInput value={props.state.secondOperand !== undefined ? props.state.secondOperand : ''} disabled={props.state.result} key='secondOperand' dispatch={props.dispatch}></ValueInput>)
+        if (props.state.result !== undefined) {
+            display.push(<span className="equal-sign" key='equal-sign'>=</span>);
+            display.push(<ValueDisplay value={props.state.result} key='result'></ValueDisplay>);
+        }
+    }
+    return (
+        <div className="header">
+            {display}
+        </div>
+    );
+};
+
+export default Header;
