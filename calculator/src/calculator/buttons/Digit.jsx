0c14416b522ccbc6eae3d90fd063267caf963f3b
Todd Gould <todd@abilsoft.com>
2021-06-23 14:53:30 -0600

    Initial baseline

---
+++ calculator/src/calculator/buttons/Digit.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './Digit.css';
+
+const Digit = (props) => {
+    const classes = `Digit ${props.state.result ? 'disabled' : ''}`;
+    return (
+        <div className={classes} onClick={e => {
+            e.preventDefault();
+            if (!props.state.result) {
+                props.dispatch({type: 'digitClick', payload: props.label})
+            }
+        }}>
+            <span>{props.label}</span>
+        </div>
+    );
+};
+
+export default Digit;
