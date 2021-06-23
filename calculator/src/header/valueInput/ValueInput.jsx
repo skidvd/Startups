a07d6673374c14dc0130d0ee62a7cfe30db99cf0
Todd Gould <todd@abilsoft.com>
2021-06-23 16:42:30 -0600

    Initial baseline

--- calculator/src/header/valueInput/ValueInput.jsx
+++ calculator/src/header/valueInput/ValueInput.jsx
@@ -5,7 +5,6 @@ import './ValueInput.css';
 const ValueInput = (props) => {
     return (
         <input type="number" className="value-input" value={props.value} onInput={e => {
-            console.log(e.target.value);
             props.dispatch({type: 'operandSet', payload: e.target.value})
         }} disabled={props.disabled}></input>
     )
