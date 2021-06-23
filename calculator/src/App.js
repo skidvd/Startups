3d8263f4d9814341c03a114206d5b2f1cf1693ab
Todd Gould <todd@abilsoft.com>
2021-06-23 15:40:48 -0600

    Initial baseline

---
+++ calculator/src/App.js
@@ -0,0 +1,26 @@
+
+import { useReducer } from 'react';
+
+import './App.css';
+
+import Header from "./header/Header";
+import Calculator from './calculator/Calculator';
+import Footer from './footer/Footer';
+import { initialState, reducer } from './calculator/state/state'
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, undefined, initialState);
+
+  return (
+    <div className="App">
+        <Header state={state} dispatch={dispatch}></Header>
+        <hr/>
+        <Calculator state={state} dispatch={dispatch}></Calculator>
+        <hr/>
+        <Footer state={state} dispatch={dispatch}></Footer>
+        <div>{JSON.stringify(state, undefined, '\t')}</div>
+    </div>
+  );
+}
+
+export default App;
